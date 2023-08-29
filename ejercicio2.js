7ce73d70d386eeea3d3b807e6fffa3ff1c11ede9
AlexMg <gonzalexm321@gmail.com>
2023-08-29 08:48:48 -0500

    Removed previous

---
+++ ejercicio2.js
@@ -0,0 +1,10 @@
+let texto = prompt("Ingrese texto")
+let conteo = 0
+
+for(let i=0; i<texto.length; i++){
+    let letra = texto[i].toLowerCase();
+    if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+        conteo++
+    }
+}
+console.log("La cantidad de vocales en el texto es: " +conteo)
---
+++ ejercicio3.js
@@ -0,0 +1,10 @@
+let nums = [5,10,12,-1,3,24]
+let mayor = 0
+
+for(let i = 0; i < nums.length; i++){
+    if(nums[i]>mayor){
+        mayor=nums[i]
+    }
+    
+}
+console.log("El numero mayor es: " +mayor)
